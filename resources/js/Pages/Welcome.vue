66f8974d18057114f3f0143c22089ade7fff0bd2
dejoyag782 <dejoyag782@gmail.com>
2024-05-12 23:00:13 +0800

    Modified the landing page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -38,11 +38,11 @@ function handleImageError() {
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#f97316] selection:text-white"
         >
-            <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3 dark:bg-gray-800 p-5">
-                    <div class="flex lg:justify-center lg:col-start-2" >
+            <div class="relative w-full">
+                <header class="flex justify-between h-16 items-center gap-5 py-10 lg:grid-cols-3 dark:bg-gray-700 px-10 py-0">
+                    <div>
                         <Link href="/">
-                            <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+                            <ApplicationLogo class="block h-20 w-auto fill-current text-gray-800 dark:text-gray-200" />
                         </Link>
                     </div>
                     <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
@@ -73,7 +73,7 @@ function handleImageError() {
                     </nav>
                 </header>
 
-                <main class="mt-6">
+                <main class="mt-6 max-w-2xl px-6 lg:max-w-7xl">
                     <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
                         <div
                             class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#f97316] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#f97316]"
